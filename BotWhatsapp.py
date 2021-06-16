a2f28bbc6e1bf2f316c0d0a34946ce2436cdedba
figueiredorodrigo <85807972+figueiredorodrigo@users.noreply.github.com>
2021-06-16 16:47:05 -0300

    Add files via upload

---
+++ BotWhatsapp.py
@@ -0,0 +1,7 @@
+import pywhatkit
+"""No primeiro campo, modificar o ddd + numero de telefone da pessoa, no segundo campo a mensagem
+    que você deseja enviar para a pessoa, no terceiro campo a hora e no quarto campo, o minuto. Após isso,
+    só aguardar o horário que vc agendou, que a mensagem vai disparar automaticamente"""
+
+
+pywhatkit.sendwhatmsg('+55dddtelefonedapessoa','Testando o Bot do Whatsapp!', 16, 37)
